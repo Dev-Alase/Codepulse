2e7f57366372c9d42144a5102c50dfe5b61d2b17
dheerajcl <cldheeraj541@gmail.com>
2023-10-31 19:50:54 +0530

    Some design implementations

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,16 @@
 // LandingPage.js
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import Typed from 'typed.js';
 import ParticleEffect from '../Particleeffect';
 import './Landingstyle.css';
+import './TextAnimation.css';
 
 const LandingPage = () => {
-  const introText = "Step into coding's fast lane with our real-time editor—your ideas, instantly tangible. No more waiting; just code, collaborate, and create with unprecedented speed.";
+  const introText = `Step into coding's fast lane with our real-time editor—your ideas, instantly tangible.No more waiting;
+Just code,
+Collaborate and
+Create with unprecedented speed.`;
 
   useEffect(() => {
     // Typed.js typing effect for the subheading
@@ -20,18 +24,41 @@ const LandingPage = () => {
     return () => {
       typedSubHeading.destroy();
     };
-  }, []); // Empty dependency array ensures that this effect runs only once after initial render
+  }, []); // Empty dependency array ensures that this effect runs only once after the initial render
 
   return (
     <div>
+      {/* SVG Title */}
+      <div className="container">
+        <svg viewBox="0 0 960 300">
+          <symbol id="s-text">
+            <text textAnchor="middle" x="50%" y="80%">Code-pulse</text>
+          </symbol>
+
+          <g className="g-ants">
+            <use xlinkHref="#s-text" className="text-copy"></use>
+            <use xlinkHref="#s-text" className="text-copy"></use>
+            <use xlinkHref="#s-text" className="text-copy"></use>
+            <use xlinkHref="#s-text" className="text-copy"></use>
+            <use xlinkHref="#s-text" className="text-copy"></use>
+          </g>
+        </svg>
+      </div>
+
+      {/* Existing Content */}
       <section className="bg-hero" id="home">
-        <div className="container">
+        <div className="container"> {/* Added text-center class */}
           <div className="row vh-md-100">
-            <div className="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center">
-            <h1 className="heading-black text-capitalize fadeInLeft">Code-pulse</h1>
+            <div className="col-md-8 col-sm-10 col-12 mx-auto my-auto">
+              {/* <h1 className="heading-black text-capitalize fadeInLeft">Code-pulse</h1> */}
               <h2 className="sub-heading text-capitalize fadeInLeft"></h2>
               <p className="lead-typed py-3 fadeInLeft">
-                <span className="type-in">{introText}</span>
+                <span className="type-in">{introText.split('\n').map((line, i) => (
+                  <span key={i}>
+                    {line}
+                    <br />
+                  </span>
+                ))}</span>
               </p>
 
               {/* Link to navigate to Home.js */}
